8b0a0933d69c7516c02becc03fcef0366b19dc39
Mina Medic <alekmina94@gmail.com>
2016-06-18 15:02:14 +0200

    sihronizacija

--- ORI_osmosmerka_web/WebContent/js/mainScriptJS.js
+++ ORI_osmosmerka_web/WebContent/js/mainScriptJS.js
@@ -1,18 +1,8 @@
 function random()
 {
-	document.getElementById("0,0").innerHtml = "A";
-//	for (var i = 0; i < 12; i++) 
-//	{
-//		for (var j = 0; j < 12; j++)
-//		{
-//			var divID = i + "," + j;
-//			//var div = document.getElementById(divID);
-//			
-//			//$(divID).val();
-//			//alert(divID);
-//			document.getElementById(divID).innerHtml = "A";
-//			
-//			$('#'+divID).html('A');
-//		}
-//	}
+	for (var i = 0; i < 12*12; i++) 
+	{
+		var divID = "#" + i;
+		$(divID).html('A');
+	}
 }
