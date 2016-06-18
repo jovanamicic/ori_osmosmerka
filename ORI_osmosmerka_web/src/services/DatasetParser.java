b6583c93a6e2f9c6fde98bb8d42ed8e14cc8011f
Jovana Micic <micicjovana94@gmail.com>
2016-06-18 23:58:18 +0200

    parser i index page izmene

---
+++ ORI_osmosmerka_web/src/services/DatasetParser.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DatasetParser {
+	
+	public static ArrayList<String> parseDataSet(String dataSet){
+		ArrayList<String> words = new ArrayList<String>();
+		String path = ".\\WebContent\\dataset\\" + dataSet;
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File(path));
+
+			while (scanner.hasNext()) {
+				String word = scanner.next().trim();
+				if (word.length() > 1)
+					words.add(word.toLowerCase());
+			}
+			scanner.close();
+		         
+		} catch (FileNotFoundException e) {
+			System.out.println("File doesn't exist.");
+		}
+        
+		return words;
+	}
+
+	public static void main(String[] args) {
+		String dataSet = "beach.csv";
+		ArrayList<String> words = parseDataSet(dataSet);
+	}
+
+}
