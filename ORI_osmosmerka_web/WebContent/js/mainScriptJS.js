e8c12139e09db3fcf9da2115fd868fe2c5d5ec4f
Katarina <katarinapreradov@gmail.com>
2016-06-18 14:30:45 +0200

    grid

---
+++ ORI_osmosmerka_web/WebContent/js/mainScriptJS.js
@@ -0,0 +1,18 @@
+function random()
+{
+	document.getElementById(divID).innerHtml = "A";
+//	for (var i = 0; i < 12; i++) 
+//	{
+//		for (var j = 0; j < 12; j++)
+//		{
+//			var divID = i + "," + j;
+//			//var div = document.getElementById(divID);
+//			
+//			//$(divID).val();
+//			//alert(divID);
+//			document.getElementById(divID).innerHtml = "A";
+//			
+//			$('#'+divID).html('A');
+//		}
+//	}
+}
