50d4a79a7a74884623b31c1b112693136707b945
student <student@localhost>
2016-06-28 11:57:57 +0200

    Selekcija slova

--- ORI_osmosmerka_web/WebContent/js/mainScriptJS.js
+++ ORI_osmosmerka_web/WebContent/js/mainScriptJS.js
@@ -1,8 +1,45 @@
+var startClick = 0; //not clicked
+var endClick = 0; //not clicked
+
 function random()
 {
 	for (var i = 0; i < 12*12; i++) 
 	{
 		var divID = "#" + i;
-		$(divID).html('A');
+		$(divID).html(i.toString().charAt(0));
 	}
 }
+
+$(document).on('mousedown','.letters',function(){
+	if (startClick == 0){
+		var selectedLetter = $(this).text();
+		$('#showSelectedLetters').val(selectedLetter);
+		$(this).css("background", "#555555");
+		startClick = 1; // clicked
+		endClick = 0;
+	}
+	else if (startClick == 1 && endClick ==0){
+		startClick = 0;
+		endClick = 1;
+		//TODO pozvati funkciju koja ce proveriti tacnost 
+		alert("check answer");
+		//ako je pogodio rec obelezi je kao pronadjenu
+		//ako nije ponisti svima boju
+		for (var i = 0; i < 12*12; i++) 
+		{
+			var divID = "#" + i;
+			$(divID).css("background", "#1abc9c");
+		}
+	}
+	
+})
+
+$(document).on('mouseover','.letters', function(){
+	if (startClick == 1 && endClick == 0){
+		var text = $('#showSelectedLetters').val();
+		var selectedLetter = $(this).text();
+		$(this).css("background", "#555555");
+		$('#showSelectedLetters').val(text + selectedLetter);
+	}
+});
+
